a39cd3669d43a25f8ab2f2c4bebc61dc9ec4a48b
LeahMeth <leahkops1@gmail.com>
2022-11-20 23:32:26 +0200

    coding challenge #3

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/codingChallenge.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/codingChallenge.js
@@ -40,3 +40,32 @@ const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(totals); */
 
 // CODING CHALLENGE #3
+const mark = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+const john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.firstName} ${mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName} ${john.lastName}'s (${john.bmi}).`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.firstName} ${john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName} ${mark.lastName}'s (${mark.bmi}).`);
+} else {
+    console.log('Equal BMIs');
+}
