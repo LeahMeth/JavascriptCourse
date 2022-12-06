e91245f19db1bb3ce1c38970b85a39a6e8fd4da7
LeahMeth <leahkops1@gmail.com>
2022-12-06 11:21:42 +0200

    DOM manipulation

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 12;
+
+// for input field, use 'value' property instead of 'textContent'
+document.querySelector('.guess').value = 17;
+console.log(document.querySelector('.guess').value);
